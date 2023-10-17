adc3b8310744ad6bff81c83ea0f5b167f5818ad2
Pugud Pambudi <noerbayu73@gmail.com>
2023-10-17 15:29:07 +0700

    update config vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/https://github.com/Pugud19/library-image/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/library-image/" : "/",
 };
